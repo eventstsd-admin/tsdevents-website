17b6c844734837c8aa914e7125e49cc609da6e8c
Dhyey Rathi <dhyeyrathi7@gmail.com>
2026-04-01 23:09:15 +0530

    build fixes

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,6 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
-    minify: 'terser',
     rollupOptions: {
       output: {
         manualChunks: undefined,
